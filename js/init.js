c3c0b04aac752823242be5acf34807adf9a0b5f2
Alessio Burgio <alessioburgio85@gmail.com>
2020-09-16 15:56:03 +0200

    improved responsiveness and added metas

--- js/init.js
+++ js/init.js
@@ -11,7 +11,7 @@
 ------------------------------------------------------ */
 
     setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	   $('h1.responsive-headline').fitText(1, { minFontSize: '20px', maxFontSize: '90px' });
 	 }, 100);
 
 
