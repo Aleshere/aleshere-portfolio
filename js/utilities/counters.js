293d6d23fc99473403e767403eed819ba64f8c8a
Aleshere <alessioburgio85@gmail.com>
2021-01-11 17:13:41 -0500

    created packaged plans

--- js/utilities/counters.js
+++ js/utilities/counters.js
@@ -1,6 +1,6 @@
 $('.counters-container').waypoint(
   function () {
-    $('.circle').each(function () {
+    $('.circle-percent').each(function () {
       var $this = $(this),
         countTo = $this.attr('data-count');
 
@@ -17,6 +17,27 @@ $('.counters-container').waypoint(
           },
           complete: function () {
             $this.text(this.countNum + '%');
+          },
+        }
+      );
+    });
+    $('.circle-int').each(function () {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+
+      $({ countNum: $this.text() }).animate(
+        {
+          countNum: countTo,
+        },
+
+        {
+          duration: 1000,
+          easing: 'linear',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
           },
         }
       );
