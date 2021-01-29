b648d5acdc8ec98a24a163cd7af3cc96082dd030
Aleshere <alessioburgio85@gmail.com>
2021-01-29 17:32:51 -0500

    finished seo

--- js/utilities/smoothScroll.js
+++ js/utilities/smoothScroll.js
@@ -18,3 +18,24 @@ $('.quick-links').on('click', '.smoothScroll', function (e) {
       }
     );
 });
+
+$('.ol-links').on('click', '.smoothScroll', function (e) {
+  e.preventDefault();
+
+  var target = this.hash,
+    $target = $(target);
+  console.log($target);
+
+  $('html, body')
+    .stop()
+    .animate(
+      {
+        scrollTop: $target.offset().top,
+      },
+      800,
+      'swing',
+      function () {
+        window.location.hash = target;
+      }
+    );
+});
