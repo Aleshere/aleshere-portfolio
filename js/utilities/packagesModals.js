2c2621125c3d95f577711ef36af7c5c2f592cad7
Aleshere <alessioburgio85@gmail.com>
2021-02-11 17:35:29 -0500

    done packages modals

--- js/utilities/packagesModals.js
+++ js/utilities/packagesModals.js
@@ -10,6 +10,9 @@ modalShow.on('click', function (e) {
   e.preventDefault();
   html.addClass('no-scroll');
   modal.addClass('is-visible');
+  $('#modalDescription').html($(this).data('description'));
+  $('#modalTitle').html($(this).data('title'));
+  $('#modalContent').html($(this).data('content'));
   demo.attr('aria-hidden', 'true');
   modal.attr({
     'aria-hidden': 'false',
@@ -23,6 +26,9 @@ modalHide.on('click', function (e) {
   e.preventDefault();
   html.removeClass('no-scroll');
   modal.removeClass('is-visible');
+  $('#modalDescription').html('');
+  $('#modalTitle').html('');
+  $('#modalContent').html('');
   demo.attr('aria-hidden', 'false');
   modal.attr('aria-hidden', 'true');
   modal.removeAttr('open tabindex');
